b0024e92ccb17322b93a5a120a7fe214cc408a7a
Dustin Pham <dustinpham07@gmail.com>
2024-11-22 17:53:49 -0800

    Completed exercise 2.9, printed list of phonebook does not update unless there is a change detected in the filtered input. Exercise does not specify requirements of the state of filtering

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const PrintAllNames = ({people}) => {
-  return (
-    <div>{people.map(person => <main>{person.name} {person.number}</main>)}</div>
-  )
-  
+    return (
+      <div>{people.map(person => <main>{person.name} {person.number}</main>)}</div>
+    )
 }
 
 const App = () => {
@@ -14,8 +13,14 @@ const App = () => {
     { id: 3, name: 'Ada Lovelace', number: '12-43-234423'}
   ]) 
 
+  const [filteredList, setFilteredList] = useState([
+    { id: 0, name: 'Arto Hellas', number: '040-123456'}, 
+    { id: -1, name: 'Sergio Meyers', number: '39-44-5323523'},
+    { id: -2, name: 'Ada Lovelace', number: '12-43-234423'}])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,6 +41,7 @@ const App = () => {
     } else {
       alert(`${personObject.name} is already in the phonebook.`)
     }
+    setFilteredList(persons)
   }
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -45,9 +51,30 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+    if (event.target.value!="") {
+      console.log("There is a filter applied")
+      const filterBySearch = persons.filter((person) => {
+        if (person.name.toString().toLowerCase().includes(event.target.value.toString().toLowerCase())) {return person}
+      }) 
+      setFilteredList(filterBySearch) 
+    } else {
+      setFilteredList(persons)
+    }
+
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with: <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+
+
+      <h2>Add a new number</h2>
       <form onSubmit={addPerson}> 
         <div>
           name: <input value={newName} onChange={handleNameChange}/> 
@@ -61,7 +88,7 @@ const App = () => {
       </form>
       <h2>People</h2>
 
-      <PrintAllNames people={persons}/> 
+      <PrintAllNames people={filteredList}/> 
     </div>
   )
 }
