eef813d6c91fdeee99a8cbd50be10e24b8a13586
Dustin Pham <dustinpham07@gmail.com>
2024-11-22 16:42:40 -0800

    Completed exercise 2.8, adding in phone numbers

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,44 +2,64 @@ import { useState } from 'react'
 
 const PrintAllNames = ({people}) => {
   return (
-    <div>{people.map(person => <main>{person.name}</main>)}</div>
+    <div>{people.map(person => <main>{person.name} {person.number}</main>)}</div>
   )
   
 }
 
-
 const App = () => {
-  const [persons, setPersons] = useState(
-    [{ id: 1, name: 'Arto Hellas' }, {id: 2,name: 'Sergio Meyers'}]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas', number: '040-123456'}, 
+    { id: 2, name: 'Sergio Meyers', number: '39-44-5323523'},
+    { id: 3, name: 'Ada Lovelace', number: '12-43-234423'}
+  ]) 
 
-  const addName = (event) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       id: String(persons.length+1),
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    console.log("Adding", event.target)
+    var same = persons.find(function(person) {
+      return person.name === personObject.name
+    })
+
+    if (!same) {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+      console.log("Added " + personObject.name)
+    } else {
+      alert(`${personObject.name} is already in the phonebook.`)
+    }
   }
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
-
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}> 
+      <form onSubmit={addPerson}> 
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange}/> 
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>People</h2>
 
       <PrintAllNames people={persons}/> 
     </div>
