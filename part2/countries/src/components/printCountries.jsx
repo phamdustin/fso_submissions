11182c5fb66dd7499e78a74ed183ee89bd2eb20b
Dustin Pham <dustinpham07@gmail.com>
2024-11-28 17:01:25 -0800

    Added show buttons for each country, need to fix how it displays information for a country, currently it shows the same information for every country

--- part2/countries/src/components/printCountries.jsx
+++ part2/countries/src/components/printCountries.jsx
@@ -1,4 +1,28 @@
+import {useState} from 'react'
+
+const ShowDetails = ({details}) => {
+    if (!details){return }
+    console.log(`ShowDetails has been clicked for ${details.name.common}`)
+    return (
+        <div>
+            <h1>{details.name.common}</h1>
+            <ul>capital {details.capital} </ul>
+            <ul>area {details.area}</ul>
+            <h3>languages:</h3>
+            {Object.values(details.languages).map((language,index) => <li key={index}>{language}</li>)}
+            <img src={details.flags.png} alt="new"/>
+        </div>  
+    )
+}
+
 const PrintCountries = (props) => {
+    const [details,setDetails] =useState('')
+
+    const detailsHandler = (event,country) => {
+        console.log(`detailsHandler has been clicked for ${country.name.common}`)
+        setDetails(country)
+    }
+
     if (!props.countriesList || props.filter==="") {
         return ""
     }
@@ -19,7 +43,16 @@ const PrintCountries = (props) => {
         )
     }
     return (
-        <div>{props.countriesList.map(country => <main>{country.name.common}</main>)}</div>
+        <div>
+            {props.countriesList.map(country => 
+                <main>{country.name.common}
+                    <button country={country} onClick={(e) => {detailsHandler(e,country)}}>show</button>
+                    <ShowDetails details={details}/>
+                    </main>)}
+            
+            
+            
+        </div>
     )
 }
 export default PrintCountries
