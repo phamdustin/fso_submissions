0b6204c1e022ae3f0ca48db8165e2c346e07bae4
Dustin Pham <dustinpham07@gmail.com>
2024-12-14 21:08:39 -0800

    Completed exercise 4.5

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -60,3 +60,70 @@ describe('total likes', () => {
     assert.strictEqual(result, 30)
   })
 })
+
+
+describe ('favorite blog', () => {
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 20,
+      __v: 0
+    },
+    {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+    }
+]
+  test('single blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+    assert.deepStrictEqual(result,listWithMultipleBlogs[1])
+  })
+
+  test('no blogs in list returns 0', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result,0)
+  })
+
+  const listWithMultipleHighestLikes = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 20,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f2',
+      title: 'Harmful Considered',
+      author: 'Dijkstra Edsger',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 20,
+      __v: 0
+    },
+  ]
+
+  test('multiple blogs with highest likes, returns one of them', () => {
+    const result = listHelper.favoriteBlog(listWithMultipleHighestLikes)
+    if ((result === listWithMultipleHighestLikes[1] || result === listWithMultipleHighestLikes[0])) {
+      assert(true)
+    } else {
+      assert(false)
+    }
+  })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,13 +3,27 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
+  // exercise 4.4
   if (!blogs[0]) {
     return 0
   }
   return blogs.map((blog) => blog.likes).reduce((a,b) => a+ b)
 }
 
+const favoriteBlog = (blogs) => {
+  // exercise 4.5
+  if (!blogs[0]) {
+    return 0
+  }
+  
+  const mostLikedBlog = blogs.reduce((maxBlog, currentBlog) => {
+    return currentBlog.likes > maxBlog.likes ? currentBlog: maxBlog
+  }, blogs[0])
+
+  return mostLikedBlog
+}
+
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
