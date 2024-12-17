8cec4eefa267aaeb4d35844552f01ff82dc3eb42
Dustin Pham <dustinpham07@gmail.com>
2024-12-16 18:22:14 -0800

    Exercise 4.9

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after} = require('node:test')
+const { test, after, describe} = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const assert = require('node:assert')
@@ -6,16 +6,27 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test.only('blogs are returned as json', async() => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe.only('blog api tests', async () => {
+  test.only('blogs are returned as json', async() => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test.only('blog count returns 1', async() => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length,1)
+  })
 
-test.only('blog count returns 1', async() => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length,1)
+  test.only('unique id is id', async () => {
+    const blogs = await api.get('/api/blogs')
+    blogs.body.forEach((blog) => {
+      assert.notStrictEqual(blog.id, undefined)
+      assert.strictEqual(blog._id,undefined)
+    })
+  })
+  
 })
 
 after(async () => {
