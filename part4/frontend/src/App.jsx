94f1a3bd39e9a867b8c294d285059b0dfd6ba459
Dustin Pham <dustinpham07@gmail.com>
2024-12-25 18:08:00 -0800

    Fixed exercise 5.6

--- part4/frontend/src/App.jsx
+++ part4/frontend/src/App.jsx
@@ -13,13 +13,6 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
 
   const [user, setUser] = useState(null)
-  const [newBlog, setNewBlog] = useState(null)
-
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
-  const [blogFormVisible, setBlogFormVisible] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -58,29 +51,12 @@ const App = () => {
     }
   }
 
-
-
-
-  const handleBlogChange = (event) => {
-    setNewBlog(event.target.value)
-  }
-
-  const addBlog = async (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: title,
-      author: author,
-      url: url,
-    }
-
+  const addBlog = (blogObject) => {
     blogService
       .create(blogObject)
       .then(returnedBlog => {
         setBlogs(blogs.concat(returnedBlog))
         setErrorMessage("new blog added!")
-        setTitle('')
-        setAuthor('')
-        setUrl('')
         setTimeout(() => {
           setErrorMessage(null)
         }, 5000)
@@ -111,11 +87,13 @@ const App = () => {
   )
 
   const blogForm = () => {
-    const hideWhenVisible = {display: blogFormVisible? 'none': ''}
-    const showWhenVisible = {display: blogFormVisible? '' : 'none'}
-
     return(
-/*       <div>
+      <div>
+        <Togglable buttonLabel='Add blog'>
+          <BlogForm createBlog={addBlog}/>
+        </Togglable>
+      </div>
+/*   <div>
         <div style={hideWhenVisible}>
           <button onClick={() => setBlogFormVisible(true)}>Add Blog</button>
         </div>
@@ -129,16 +107,17 @@ const App = () => {
         </div>
         <button onClick={() => setBlogFormVisible(false)}>Cancel</button>
       </div> */
-      <div>
+/*       <div>
         <Togglable buttonLabel='Add blog'>
-        <BlogForm
-            title={title} author={author} url={url} addBlog={addBlog}
-            handleTitleChange={({target}) => setTitle(target.value)}
-            handleAuthorChange={({target}) => setAuthor(target.value)}
-            handleUrlChange={({target}) => setUrl(target.value)}
-          />
+          <BlogForm
+              title={title} author={author} url={url} addBlog={addBlog}
+              handleTitleChange={({target}) => setTitle(target.value)}
+              handleAuthorChange={({target}) => setAuthor(target.value)}
+              handleUrlChange={({target}) => setUrl(target.value)}
+            />
         </Togglable>
-      </div>
+      </div> */
+
     )
   }
 
--- part4/frontend/src/components/BlogForm.jsx
+++ part4/frontend/src/components/BlogForm.jsx
@@ -1,40 +1,50 @@
-const BlogForm = ({
-  title,
-  author,
-  url,
-  addBlog,
-  handleTitleChange,
-  handleAuthorChange,
-  handleUrlChange
-}) => {
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog}) => {
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: title,
+      author: author,
+      url: url,
+    })
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
   return (
-    <div>
+     <div>
       <h2>Submit new blog</h2>
       <form onSubmit={addBlog}>
         <div>
           Title: <input
             value={title}
             name="Title"
-            onChange={handleTitleChange}
+            onChange={event => setTitle(event.target.value)}
           />    
         </div>
         <div>
           Author: <input 
             value={author} 
             name="Author" 
-            onChange={handleAuthorChange}
+            onChange={event => setAuthor(event.target.value)}
           />
         </div>
         <div>
           Url: <input
             value={url}
             name="Url"
-            onChange={handleUrlChange}
+            onChange={event => setUrl(event.target.value)}
           />
         </div>
         <button type="submit">Submit</button>
       </form>
-    </div>
+    </div> 
   )
 }
 export default BlogForm
