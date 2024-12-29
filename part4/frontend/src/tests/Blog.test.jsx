acbb174d35bb7b9db7a51ba7b6fea6a9e3aaf75e
Dustin Pham <dustinpham07@gmail.com>
2024-12-29 14:37:46 -0800

    Exercise 5.14

--- part4/frontend/src/tests/Blog.test.jsx
+++ part4/frontend/src/tests/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
 // Tests that the component renders the blog's title and author, but the rest will not be rendered
@@ -21,4 +22,30 @@ test('renders content', () => {
   const element3 = screen.queryByText('7')
   expect(element3).toBeNull()
   
+})
+
+test('checks that url and likes will be rendered after clicking the button', async() => {
+  const blog = {
+    title: 'Rendering test',
+    author: 'admin',
+    url: 'willBeRendered.com',
+    likes: 7,
+    user: {
+      name: 'admin2.0'
+    }
+  }
+
+
+  render(
+    <Blog blog={blog} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  const element = screen.getByText('willBeRendered.com')
+  expect(element).toBeDefined()
+  const element2 = screen.queryByText('7')
+  expect(element2).toBeDefined()
 })
