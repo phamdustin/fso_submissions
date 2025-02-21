ec3c4a81ed5f6b8a202bb2b5f2c0f5ef51bd0bbf
Dustin Pham <dustinpham07@gmail.com>
2025-02-20 16:50:28 -0800

    Exercise 6.20

--- part6/query-anecdotes-main/src/App.jsx
+++ part6/query-anecdotes-main/src/App.jsx
@@ -1,19 +1,39 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
 const App = () => {
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
+/*   const anecdotes = [
     {
       "content": "If it hurts, do it more often",
       "id": "47145",
       "votes": 0
     },
-  ]
+  ] */
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result.isError ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
