323d2b01210585a8224ef4b973d315699a7224b3
Dustin Pham <dustinpham07@gmail.com>
2025-02-24 16:41:38 -0800

    Exercise 7.1

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter as Router,
+  Routes, Route, Link,
+} from 'react-router-dom'
 
-const Menu = () => {
+/* const Menu = () => {
   const padding = {
     paddingRight: 5
   }
   return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
+    <Router>
+      <div>
+        <Link to="/anecdotes" style={padding}>anecdotes</Link>
+        <Link to="/create-new" style={padding}>create new</Link>
+        <Link to='about' style={padding}>about</Link>
+      </div>
+    </Router>
   )
-}
+} */
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
@@ -121,15 +126,28 @@ const App = () => {
 
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
-
+  const padding = {
+    paddingRight: 5
+  }
   return (
+
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+      {/* <Menu /> */}
+      <Router>
+        <div>
+          <Link to="/" style={padding}>anecdotes</Link>
+          <Link to="/create" style={padding}>create new</Link>
+          <Link to='about' style={padding}>about</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/>} />
+          <Route path="/create" element={<CreateNew addNew={addNew} />} />
+          <Route path="/about" element={<About />} />
+
+        </Routes>
+      </Router>
+      <Footer></Footer>
     </div>
   )
 }
